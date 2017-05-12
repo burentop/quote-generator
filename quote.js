81a9f0babbd2625894c5c401dd049730cc2b3807
Brent Perry <burentoperii@gmail.com>
2017-05-12 09:48:52 -0500

    complete styling and functionality

---
+++ quote.js
@@ -0,0 +1,28 @@
+var quoteText = "";
+var quoteAuthor = "";
+
+$(document).ready(function() {
+	getQuote();
+	$("#getMessage").on("click", function(event) {
+		event.preventDefault();
+		getQuote();
+	});
+	$("#tweet").on("click", function(event) {
+		event.preventDefault();
+		window.open("https://twitter.com/intent/tweet?text=" + quoteText + " - " + quoteAuthor);
+	});
+});
+
+function getQuote() {
+	$.ajax({
+	  type: 'GET',
+	  dataType: 'json',
+	  cache: false,
+	  url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?',
+	  success: function(data){
+	  	quoteText = data.quoteText;
+	  	quoteAuthor = data.quoteAuthor;
+	    $("#displayedQuote").html("<div class='quoteText'>" + quoteText + "</div><br>" + "<div class='quoteAuthor'>" + quoteAuthor + "</div>");
+	  }
+	});
+}
